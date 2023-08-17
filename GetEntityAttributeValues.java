33091429020eabd22bf0235d510c0a52f7b209b5
Daniel <124480168+DanielHzp@users.noreply.github.com>
2023-08-16 21:30:04 -0500

    Tracing and get entity values syntax

---
+++ GetEntityAttributeValues.java
@@ -0,0 +1,23 @@
+
+
+
+//Obtain attribute value using an XPath to navigate to the attribute (XPath starting entity depends on the business rule context/scope)
+var AttributeValuetoObtain=<mMasterEntityName.Attribute>;
+
+//Obtain attribute value using contextualized XPath method
+//Me object refers to the business rule context/scope (Process or entity level)
+var AttributeValuetoObtain=Me.getXPath("mMasterEntityName.Attribute");
+
+//Obtain attribute value when XPath doesen´t start from process entity (always use it inside collection form rules)
+var AttributeValuetoObtain=Me.Context.getXPath("mMasterEntityName.Attribute");
+
+
+//Obtain attribute value using contextualized XPath method
+//
+
+var CustomMe=<mMasterEntityName.kmForeignKey>;
+
+var AttributeValuetoObtain=CustomMe.getXPath("")
+
+//Method to obtain the value of an attribute of an entity in business rules or process level expressions
+
---
+++ Tracing Debug.java
@@ -0,0 +1,34 @@
+
+
+
+// Trace syntax to log and debug code in business rules or process level expressions
+// CHelper class method 
+
+
+
+//Variable that stores an XPath, object, variable or attribute value that will be traced when the code is compiled
+var VariableToTrace= 'Variable/object/attribute/XPath to trace';
+
+//Create trace file name concatenating a case number identifier (optional)
+var traceFileName= "Insert file name where trace will be stored " + Me.Case.CaseNumber;
+
+CHelper.trace(traceFileName, "Name of the XPath/variable/object/attribute to log: "+ VariableToTrace); 
+
+
+
+
+//Example using more than one variable in a single trace
+
+var VariableToTrace1 = Me.Case.CaseNumber;
+
+var VariableToTrace2 = "sStringVariable ='"+sStringVariable+"'";
+
+CHelper.trace("Insert file name where trace will be stored", "Sample text VariableToTrace1: "+VariableToTrace1+", Smaple text VariableToTrace2: "+VariableToTrace2);
+
+
+
+//Example using multiple variables and XPaths in a single trace
+
+var traceName = "Insert file name "+Me.Case.CaseNumber+".FileExtension";
+
+CHelper.trace(traceName, "Sample text, XPath 1 to trace: " + <MasterEntityName.kmForeingKey.Attribute> + " additional sample text XPath 2:" + <MasterEntityName.Attribute>);
