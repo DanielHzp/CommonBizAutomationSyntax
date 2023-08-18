6d036c63805bbdb7b961abaf2bcd82c6f1180243
Daniel <124480168+DanielHzp@users.noreply.github.com>
2023-08-17 19:39:40 -0500

    Update GetEntityAttributeValues.java

--- GetEntityAttributeValues.java
+++ GetEntityAttributeValues.java
@@ -6,19 +6,23 @@ var AttributeValuetoObtain=<mMasterEntityName.Attribute>;
 
 //Obtain attribute value using contextualized XPath method
 //Me object refers to the business rule context/scope (Process or entity level)
-var AttributeValuetoObtain=Me.getXPath("mMasterEntityName.Attribute");
+var AttributeValuetoObtain=Me.getXPath("mMasterEntityName.AttributeName");
 
 //Obtain attribute value when XPath doesen´t start from process entity (always use it inside collection form rules)
-var AttributeValuetoObtain=Me.Context.getXPath("mMasterEntityName.Attribute");
+var AttributeValuetoObtain=Me.Context.getXPath("mMasterEntityName.AttributeName");
 
 
 //Obtain attribute value using contextualized XPath method
-//
+//CustomMe is an object (foreign or primary key) that refers to a context different than the business rule default context (Me)
+//CustomMe lets the method getXPath navigate the data model starting from the foreign entity
+var CustomMe=<mMasterEntityName.kmForeignEntityKey>;
 
-var CustomMe=<mMasterEntityName.kmForeignKey>;
+var AttributeValuetoObtain=CustomMe.getXPath("ForeignEntityAttributeName");
 
-var AttributeValuetoObtain=CustomMe.getXPath("ForeignEntityAttribute");
 
-//Method to obtain the value of an attribute of an entity in business rules or process level expressions
+//CHelper Method to obtain a SPECIFIC value of an attribute of an entity in business rules or process level expressions
+// It can fetch data from any type of entity (System, parametric or master)
+var AttributeValuetoObtain= CHelper.getEntityAttrib("EntityName","AttributeName(ColumnName)","sStringAttribute = '" + sStringVariable + "'");
 
-g
+var AttributeValuetoObtain= CHelper.getEntityAttrib("EntityName","AttributeName(ColumnName)","iIntegerAttribute = " + iIntegerVariable );
+
