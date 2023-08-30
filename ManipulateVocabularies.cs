10889870e3ad9a8dddd8116da6f85ac1419540dd
Daniel <124480168+DanielHzp@users.noreply.github.com>
2023-08-30 16:07:05 -0500

    Update ManipulateVocabularies.cs

--- ManipulateVocabularies.cs
+++ ManipulateVocabularies.cs
@@ -2,11 +2,11 @@
 
 //The standard way to obtain a vocabulary value thats administered from the work portal
 //Process level invoke
-var vocabularyValue=CHelper.resolveVocabulary(Me,"vocabularyName");
+var vocabularyValue=CHelper.resolveVocabulary( Me , "vocabularyName");
 
 
 //The uncontextualized waty to obtain a vocabulary values thats administered from the work portal 
 //Global level invoke
-
+var contractNotifDays=CHelper.resolveVocabulary( null , "vocabularyName");
 
 
