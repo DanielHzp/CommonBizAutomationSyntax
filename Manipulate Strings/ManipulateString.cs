15ad217e3f104699a2924dbf0bcaf04932358e9a
Daniel <124480168+DanielHzp@users.noreply.github.com>
2024-10-07 12:50:09 -0500

    Update ManipulateString.cs

--- Manipulate Strings/ManipulateString.cs
+++ Manipulate Strings/ManipulateString.cs
@@ -1,12 +1,16 @@
 
 
-//String values manipulation in business rules sample syntax
+//STRING VALUES MANIPULATION IN BUSINESS RULES SAMPLE SYNTAX
 
 //Convert DateTime Object into a String
 var date = DateTime.Today;
 var yearDate = date.Year;
 var stringDate = yearDate.ToString();
 
+
+//Remove all leading and trailing wite spaces from a string variable
+var sModifiedString = StringValue.Trim().ToLower();
+	
 
 //Add a new line (enter command) in a string value contatenated
 
@@ -20,6 +24,7 @@ sStringValue += "Sample text string value ("+sStringValue+"), additional text co
 var sModifiedString = StringValue.Trim().ToLower();
 
 
+
 //Obtain task/activity/event display name of a current process instance
 var taskDisplayName = Me.Task.DisplayName;
 
@@ -29,13 +34,25 @@ if (taskDisplayName == "Task Display Name ")
 }
 
 
+
 //Replace a specific part of a string with a custom String value
 var sModifiedString = sModifiedString.Replace( "Specify the string or characters to BE REPLACED" , "Specify which string/characters will REPLACE"); 
+
+
+
+//USE A LIBRARY RULE TO REPLACE A STRING VALUE (LEFT ARG) WITH INPUT STRING (RIGHT ARG)
+
+FamilyRuleName.LibraryRuleName(Me, body , "[STRING_TAG]",<mProcessEntity.kmForeingKey.sStringAttribute>);
+//REPLACE ALL [STRING_TAG]" IN A TEXT BODY WITH THE STRING VALUE OF <mProcessEntity.kmForeingKey.sStringAttribute>
+
+
 
 
 //Replace spaces between characters with html space command
 sStringValue = "https://URLSample="+variable1+"+|+"+variable2+"+|+"+variable3+"";
 sStringValue = sStringValue.Replace(" ", "%20");
+
+
 
 
 
@@ -53,11 +70,15 @@ var sModifiedString = sModifiedString.Replace( StringPartToReplace, "["+sModifie
 
 
 
+
+
 //Simple concatenation of two string value variables
 var firstName = Array[i].getXPath("sFullName");
 var lastName = Array[i].getXPath("sLastName");
 
 var fullName = firstName + " " + lastName;
+	
+	
 	
 	
 	
@@ -68,7 +89,9 @@ var StringValue = ExampleArray[i].getXPath("sStringAttributeName");
 if(StringValue.Contains('@'))  //If the string variable contains the character @
 	{
 		
+		
 	var sModifiedString = StringValue.Replace(StringPartToReplace, "mailto:"+ stringVariable);  //Replace the StringPsrtToReplace with the second parameter in the variable: StringValue
+	
 	
 	//Replace a string part with a multiple string concatenation
 	sModifiedString = StringValue.Replace(StringPartToReplace, Text1 + StringValueVariable + Text2 + ExampleArray[i].getXPath("sStringAttributeName") + Text3);
@@ -82,7 +105,7 @@ if(StringValue.Contains('@'))  //If the string variable contains the character @
 	
 
 	
-	//Regular expressions handling sample
+	//REGULAR EXPRESSIONS MANIPULATION SAMPLE
 	//oRegexStringValue is a variable that stores a regular expression command to verify if a string attribute has a valid email format
 	var oRegexStringValue = new System.Text.RegularExpressions.Regex("^[[_A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[_A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?$");
 	
