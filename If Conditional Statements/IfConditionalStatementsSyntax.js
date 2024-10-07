b996102164645a17149cf60f049c42022d5032e8
Daniel <124480168+DanielHzp@users.noreply.github.com>
2024-10-07 13:10:38 -0500

    Create IfConditionalStatementsSyntax.js

---
+++ If Conditional Statements/IfConditionalStatementsSyntax.js
@@ -0,0 +1,15 @@
+
+
+// if Conditional syntax in Bizagi    -  || ,  &&  , < , >, == , !=
+//XPath navigation depends on business rule/expression context
+if( <mProcessEntityName.kpForeignKeyParamEntity.sStringAttributeName>== 'sStringValue'){
+	
+}
+
+
+
+
+
+// If conditional syntax using ternary operators
+//XPath navigation depends on business rule/expression context
+var comments = <mProcessEntityName.sStringAttributeName> == null ? "" : <mProcessEntityName.sStringAttributeName>;
