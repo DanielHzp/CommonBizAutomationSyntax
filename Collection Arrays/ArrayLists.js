9c637786ef2088ed620c935a6f653bc0d716f2a3
Daniel <124480168+DanielHzp@users.noreply.github.com>
2025-01-30 13:56:53 -0500

    Update ArrayLists.js

--- Collection Arrays/ArrayLists.js
+++ Collection Arrays/ArrayLists.js
@@ -1,7 +1,40 @@
 
+//ARRAYLISTS CREATED IN BIZAGI STUDIO BUSINESS RULES CONTAIN ONE COLUMN
 
 //DECLARE AN ARRAYLIST AND ADD ITEMS INSIDE A BUSINESS RULE
 
 var arrayListName = new ArrayList();
 
 arrayListName.Add(VariableName);
+
+
+
+
+//VALIDATE IF AN ARRAYLIST CONTAINS A VALUE IN ONE OF IT'S RECORDS
+
+var variableValue=123456
+
+if(!arrayListName.Contains(variableValue)) //IF THE ARRAYLIST DOESN'T HAVE THE SPECIFIED VALUE
+{
+		arrayListName.Add(VariableName);
+
+}
+
+
+//OBTAIN THE ARRAYLIST sizeToContent
+
+var iArrayListSize=arrayListName.Count;
+
+
+
+
+//LOOP OVER ARRAYLIST RECORDS
+
+for(var j=0;j<iArrayListSize;j++)
+{
+		//EXTRACT SURROGATE KEY ID FROM EACH RECORD IN THE ARRAYLIST
+			var iIdCasetoCreate=arrLCMNewCases[j].toString(); 
+			
+}
+
+
